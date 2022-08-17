4d951cb71363544a66e2dc46655b7689683c1fda
victus-i7 <109217433+victus-i7@users.noreply.github.com>
2022-08-17 11:26:03 +0500

    Add files via upload

---
+++ homework4/script.js
@@ -0,0 +1,196 @@
+"use strict"
+// |||||| 1 ||||||
+
+// const k = +prompt('k')
+
+// const n = +prompt('n')
+
+// for (let i = 1; i <= k; i++) {
+
+//     console.log(k);
+// }
+
+// |||||| 2 ||||||
+
+// const a = +prompt("RAQAM KIRITING");
+// const b = +prompt("RAQAM KIRITING RAQAMINGIZ A DAN KAM BO'LSIN");
+
+// for (let i = b; i <= a; i++) {
+//     console.log(i)
+// }
+
+
+// |||||| 3 ||||||
+
+// const a = +prompt("RAQAM KIRITING");
+// const b = +prompt("RAQAM KIRITING RAQAMINGIZ A DAN KAM BO'LSIN");
+
+// for (let i = a; i > b; i--) {
+//     if (i === a)
+//         continue
+//     console.log(i)
+// }
+
+
+// |||||| 4 ||||||
+
+// let kg = +prompt('Nechi kg kerak')
+
+// switch (kg) {
+//     case 1: console.log('10.000som'); break;
+//     case 2: console.log('20.000som'); break;
+//     case 3: console.log('30.000som'); break;
+//     case 4: console.log('40.000som'); break;
+//     case 5: console.log('50.000som'); break;
+//     case 6: console.log('60.000som'); break;
+//     case 7: console.log('70.000som'); break;
+//     case 8: console.log('80.000som'); break;
+//     case 9: console.log('90.000som'); break;
+//     case 10: console.log('100.000som'); break;
+//     default: console.log("10 kg dan ortig'i yoq")
+// }
+
+
+
+// |||||| 5 ||||||
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 1 == 1)
+//         continue
+//     console.log(i);
+//     console.log(`${i / 10}kg konfet ${i / 10 * 10000}`);
+
+// }
+
+
+// |||||| 6 ||||||
+
+// for (let i = 12; i <= 20; i++) {
+//     if (i % 1 == 1)
+//         continue
+//     console.log(i);
+//     console.log(`${i / 10}kg konfet ${i / 10 * 10000}`);
+
+// }
+
+
+// |||||| 7 ||||||
+
+// const a = +prompt("RAQAM KIRITING");
+// const b = +prompt("RAQAM KIRITING RAQAMINGIZ A DAN KOTTA BO'LSIN");
+// let с = 0;
+
+// for (let i = a; i <= b; i++) {
+//     с += i
+// }
+// console.log(`a bilan b ni orasidagi yeg'indisi ${с} ga teng`);
+
+
+
+// |||||| 8 ||||||
+
+// const a = +prompt("RAQAM KIRITING");
+// const b = +prompt("RAQAM KIRITING RAQAMINGIZ A DAN KOTTA BO'LSIN");
+// let c = 1;
+
+// for (let i = a; i <= b; i++) {
+//     c *= i
+// }
+// console.log(`a bilan b ni orasidagi yeg'indisi ${c} ga teng`);
+
+
+
+
+// |||||| 9 ||||||
+
+
+// const a = +prompt("RAQAM KIRITING")
+
+// for (let i = 1; i <= a; i++) {
+//     console.log(`1 dan boshlab yozgan raqamingizgacha kvadrati yozilgan ${i * 2}`);
+// }
+
+
+// |||||| 10 ||||||
+
+// CHUNMADIM
+
+
+
+// |||||| 11 ||||||
+
+// CHUNMADIM
+
+// const a = +prompt("RAQAM KIRITING")
+
+// for (let i = 1; i <= a; i++) {
+//     if (i % 2 == 1)
+
+//         console.log(i);
+// }
+
+
+// |||||| 12 ||||||
+
+// const a = +prompt("RAQAM KIRITING")
+
+// for (let i = 1; i <= a; i++) {
+//     if (i % 2 == 0)
+
+//         console.log(i);
+// }
+
+
+
+// |||||| 13 ||||||
+
+// CHUNMADIM
+
+// |||||| 14 ||||||
+
+// CHUNMADIM
+
+// |||||| 15 ||||||
+
+
+// let a = +prompt("RAQAM KIRITING")
+
+// for (let i = a; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// |||||| 16 ||||||
+
+// let n = +prompt("RAQAM KIRITING");
+
+// for (let i = 0; i <= n; i++) {
+//     if (i % 5 == 0)
+//         continue
+//     console.log(i);
+// }
+
+
+
+// |||||| 17 ||||||
+// let n = +prompt("RAQAM KIRITING");
+// let c = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     c *= i
+// }
+// console.log(`n berilgan soningizgacha ko'paytmasi ${c} ga teng`);
+
+
+// |||||| 18 ||||||
+
+
+// CHUNMADIM
+
+
+// |||||| 19 ||||||
+
+
+// CHUNMADIM
+
+
